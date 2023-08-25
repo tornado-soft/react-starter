318039cb177bf595ac615ac6cafa7221baba9c18
Bruce <recall4056@gmail.com>
2023-08-25 15:50:22 +0800

    chore: front-end engineering

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import viteLogo from '/favicon.svg'
 
 export default function App() {
   const [count, setCount] = useState(0)
